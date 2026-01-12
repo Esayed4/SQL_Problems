b3c476865856e8b5085ef5dee05ce84ef35b07d0
Elsayed Nabil <nabilesayed98@gmail.com>
2026-01-12 23:19:28 +0200

    solve Sorting and Grouping medium problems

--- LeetCode/medium/1045 Customers Who Bought All Products.sql
+++ LeetCode/medium/1045 Customers Who Bought All Products.sql
@@ -1,14 +1,5 @@
-/* Write your PL/SQL query statement below */
 
-with unique_cp as(
-    select 
-        customer_id 
-    from Customer
-    group by customer_id , product_key
-)
-
-select 
-customer_id
-from unique_cp
-having count(*)=(select count(*) from Product  )
+select customer_id 
+from Customer 
 group by customer_id
+having count(distinct product_key ) = (select count(*) from Product )
